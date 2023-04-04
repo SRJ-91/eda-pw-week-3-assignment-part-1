de80f6aabdef8be764be20bea87ef4869559d2eb
SRJ-91 <Sawyerjen@hotmail.com>
2023-04-04 14:09:20 -0500

    Done

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -91,13 +91,19 @@ console.log('Updated food list is', foodArray);
 
 // 4.f (STRETCH) TODO: Sort your favoriteFoods array
 //     in reverse alphabetical order. Log the array.
-
+foodArray.sort();
+foodArray.reverse();
+console.log('reverse list is', foodArray)
 // 4.g (STRETCH) TODO: Convert your array to a string
 //     putting the word "and" between each item.
 //     eg "tacos and pizza and pasta". Log the string.
-
+const stringyFood = foodArray.join(' and ');
+console.log("Would you like some", stringyFood);
 // 4.h (STRETCH) TODO: Make a new array that combines
 //     the favorite foods array with the animals array.
 //     Then log the new array.
 //     It should look something like:
 //     ['pizza', 'pasta', 'fish', 'cat', 'bird', 'dog']
+
+const bothArrays = animalArray.concat(foodArray);
+console.log(bothArrays);
