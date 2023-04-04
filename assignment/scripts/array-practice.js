e8915923391069ca386c299ba3c4e70771ed7fa8
SRJ-91 <Sawyerjen@hotmail.com>
2023-04-04 13:07:09 -0500

    up to 4c complete

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -49,7 +49,8 @@ animalArray.push('penguin');
 console.log('Added an animal to end,', animalArray);
 
 // 4.a. TODO: Add a new food at the end of your array & log the array
-
+foodArray.push('rice');
+console.log('Added a food to end,', foodArray);
 
 
 // Example: Remove the last animal by using Array.pop
@@ -59,14 +60,17 @@ console.log('The animals are now', animalArray);
 
 // 4.b. TODO: Remove the food at the end of your array & 
 //      log both the food removed and the updated array
-
+let removedFood = foodArray.pop();
+console.log('We removed the last food', removedFood);
+console.log('The food list is once again', foodArray);
 
 // Example: Add an animal to the beginning using Array.unshift
 animalArray.unshift('walrus');
 console.log(`Added an animal to beginning: ${animalArray}`);
 
 // 4.c. TODO: Add a food at the beginning of the array & log the array
-
+foodArray.unshift('carrot');
+console.log('Added a food to the beginning:', foodArray);
 
 // Example: Remove the first animal using Array.shift
 removedAnimal = animalArray.shift();
